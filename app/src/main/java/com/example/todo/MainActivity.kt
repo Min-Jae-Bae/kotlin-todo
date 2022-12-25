1164102f253594933fb6bf9ce5b44c85e8bf2f3c
Min-Jae-Bae <qoalswo123@icloud.com>
2022-12-25 22:43:31 +0900

    feat: setup Dagger-Hilt

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -11,31 +11,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todo.ui.theme.TodoTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+/*AndroidEntryPoint
+* 해당 클래스의 Dependency Container(비유: 물류센터) 생성하는 Annotation
+* 즉 Hilt Component 의존성들을 지정 클래스(AndroidEntryPoint 클래스)로 보낼 수 있는 집합 저장소(Container)라고 볼 수 있다.
+* */
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TodoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TodoTheme {
-        Greeting("Android")
     }
 }
