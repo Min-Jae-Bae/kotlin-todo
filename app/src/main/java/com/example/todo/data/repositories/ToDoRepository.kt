536d99f663ba8fb7247737e8c6853b8b2908adf7
Min-Jae-Bae <qoalswo123@icloud.com>
2022-12-26 00:51:06 +0900

    feat: create ToDoRepository

---
+++ app/src/main/java/com/example/todo/data/repositories/ToDoRepository.kt
@@ -0,0 +1,45 @@
+package com.example.todo.data.repositories
+
+import com.example.todo.data.ToDoDao
+import com.example.todo.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+/*Repository Pattern
+* 1. Business Logic (Entity)와 Data Access Object(DAO)를 분리 -> 관심사의 분리
+* 2. 중앙 집중 처리 방식 - 일관된 Logic 및 Data 제공
+* 3. Data encapsulation
+*
+* ToDoRepository - Repository 생성시 DAO가 필요함을 Hilt에게 알려줌 */
+class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
+
+    val getAllTasks: Flow<List<ToDoTask>> = toDoDao.getAllTasks()
+    val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
+    val sortByHighPriority: Flow<List<ToDoTask>> = toDoDao.sortByHighPriority()
+
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask> {
+        return toDoDao.getSelectedTask(taskId = taskId)
+    }
+
+    suspend fun addTask(toDoTask: ToDoTask) {
+        toDoDao.addTask(toDoTask = toDoTask)
+    }
+
+    suspend fun updateTask(toDoTask: ToDoTask) {
+        toDoDao.updateTask(toDoTask = toDoTask)
+    }
+
+    suspend fun deleteTask(toDoTask: ToDoTask) {
+        toDoDao.deleteTask(toDoTask = toDoTask)
+    }
+
+    suspend fun deleteAllTasks() {
+        toDoDao.deleteAllTasks()
+    }
+
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>> {
+        return toDoDao.searchDatabase(searchQuery = searchQuery)
+    }
+
+
+}
